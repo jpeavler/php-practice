ebc8adc307bf94005c1583e84add61b352427965
Joshua Peavler <joshuaapeavler@gmail.com>
2020-07-16 19:03:15 -0600

    Initial exploration of php

---
+++ site.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $name = "Josh"; 
+        echo("<h1>$name's Site</h1>");
+        echo("<hr></hr>");
+        echo("<p>This is my site using php</p>");
+    ?>
+</body>
+</html>

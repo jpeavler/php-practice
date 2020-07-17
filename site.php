fd3adbf685ae663491e4c2647ccca64e2558922e
Joshua Peavler <joshuaapeavler@gmail.com>
2020-07-17 08:27:30 -0600

    Added simple function to practice page

--- site.php
+++ site.php
@@ -17,6 +17,12 @@
         // echo strlen($name);
         // echo str_replace("Giraffe", "Helio", $school);
         // echo substr($school, 8, 3);
+            //Array//
+        // $fruit = array("Apple", "Orange", "Banana");
+        // $fruit[0];               would equal "Apple"
+        // $grades = array("Jim"=>"A", "Bill"=>"B-", "Bob"=>"C+");
+            //Associative Array//
+        // $grades["Jim"];          would equal "A"
     ?>
     <form action="site.php" method="post">
     <!-- Get will make this value visible in the URL, use post method instead for secure info -->
@@ -26,5 +32,14 @@
     </form>
     Your Name is <?php echo $_POST["name"]?>
     Your Age is <?php echo $_POST["age"]?>
+
+    <?php 
+        function sayHi ($name){
+            echo "Hello $name";
+        }
+        sayHi("Bill");
+        sayHi("Bob");
+        sayHi("John");
+    ?>
 </body>
 </html>
